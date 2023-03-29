c74d661baf63ad3221f2d672712c80a966ed757c
David <david.schote@ptb.de>
2023-03-29 16:41:04 +0200

    Python setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='console',
+  version='0.0.1',
+  author='David Schote',
+  author_email='david.schote@ptb.de',
+  packages=find_packages(),
+  description='Low field MRI console application with ScanHub-SDK and interface to Spectrum Instrumentation',
+)
