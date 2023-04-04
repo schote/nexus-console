6790f2e1b12156b2a8c41f484128e3148d1ad78d
David Schote <david.schote@ptb.de>
2023-04-04 10:13:38 +0200

    Extend demo

--- examples/device_interface_demo.py
+++ examples/device_interface_demo.py
@@ -3,8 +3,14 @@
 import os
 import yaml
 from pprint import pprint
+import plotly.express as px
+import plotly.io as pio
 
-from console.spcm_control.device import yaml_loader
+from console.utilities.io import yaml_loader
+from console.pulseq_interpreter.sequence import SequenceWrapper
+
+# Plotly configuration
+pio.renderers.default = "notebook"
 
 # %%
 # Load device configuration
@@ -29,5 +35,22 @@ for dev in devices:
 tx_card = list(filter(lambda device: device.__name__ == "TxCard", devices))[0]
 
 tx_card.init_card()
+
+# %%
+# Read system config
+system = config["system"][0]
+print(f"Found system configuration: {type(system)}")
+
+
+# %%
+# Load sequence and modulate RF block 
+seq = SequenceWrapper(system)
+seq_path = os.path.normpath("/Users/schote01/code/spectrum-pulseq/examples/pulseq/fid.seq")
+seq.read(seq_path)
+
+rf_mod, t = seq.get_modulated_rf_block(1)
+
+fig = px.line(x=t, y=rf_mod, title='Sinc RF Pulse', labels=dict(x="Time (us)", y="RF Amplitude"))
+fig.show()
 
 # %%
