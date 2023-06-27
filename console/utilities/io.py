715e0a52f67620cb3122913e44edfaf77ec0a17b
David Schote <david.schote@ptb.de>
2023-06-27 17:35:18 +0200

    Added depth to yaml constructor mapping

--- console/utilities/io.py
+++ console/utilities/io.py
@@ -1,10 +1,10 @@
 import yaml
 
 from console.spcm_control.device import TxCard, RxCard
-from pypulseq.opts import Opts
+# from pypulseq.opts import Opts
 
 # Add constructors to PyYAML loader
 yaml_loader = yaml.SafeLoader
-yaml_loader.add_constructor("!TxCard", lambda loader, node: TxCard(**loader.construct_mapping(node)))
-yaml_loader.add_constructor("!RxCard", lambda loader, node: RxCard(**loader.construct_mapping(node)))
-yaml_loader.add_constructor("!System", lambda loader, node: Opts(**loader.construct_mapping(node)))
+yaml_loader.add_constructor("!TxCard", lambda loader, node: TxCard(**loader.construct_mapping(node, deep=True)))
+yaml_loader.add_constructor("!RxCard", lambda loader, node: RxCard(**loader.construct_mapping(node, deep=True)))
+# yaml_loader.add_constructor("!System", lambda loader, node: Opts(**loader.construct_mapping(node)))
