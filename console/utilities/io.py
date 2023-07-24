89dfd6aa64457f25876942a11ff5d9efd68f31c2
berksilemek <berk.silemek@ptb.de>
2023-07-24 15:12:37 +0200

    VS configuration with David and pull from main

--- console/utilities/io.py
+++ console/utilities/io.py
@@ -10,7 +10,7 @@ yaml_loader = yaml.SafeLoader
 yaml_loader.add_constructor(
     "!TxCard", lambda loader, node: TxCard(**loader.construct_mapping(node, deep=True))
 )
-yaml_loader.add_constructor(
-    "!RxCard", lambda loader, node: RxCard(**loader.construct_mapping(node, deep=True))
-)
+# yaml_loader.add_constructor(
+#     "!RxCard", lambda loader, node: RxCard(**loader.construct_mapping(node, deep=True))
+# )
 # yaml_loader.add_constructor("!System", lambda loader, node: Opts(**loader.construct_mapping(node)))
