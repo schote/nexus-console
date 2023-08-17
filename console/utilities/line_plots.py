1c68d7d7d08ebd7eaa5d8b75164f81b06f10fa7f
David Schote <david.schote@ptb.de>
2023-08-17 18:20:53 +0200

    Channel wise min-max in sequence plot

--- console/utilities/line_plots.py
+++ console/utilities/line_plots.py
@@ -4,12 +4,16 @@ import numpy as np
 def plot_spcm_data(data: np.ndarray, num_channels: int):
     
     fig, ax = plt.subplots(num_channels, 1, figsize=(16, 9))
-    minmax = (data[:].min(), data[:].max()*1.10)
+    
+    # Extract min-max values per channel
+    minmax = [(data[k::num_channels].min(), data[k::num_channels].max()) for k in range(num_channels)]
+    # Add +-10% of absolute to the calculated limits
+    minmax = [(val[0] - abs(val[0])*0.1, val[1] + abs(val[1])*0.1) for val in minmax]
     
     for k in range(num_channels):
         ax[k].plot(data[k::num_channels])
         ax[k].set_ylabel("Channel {}".format(k+1))
-        ax[k].set_ylim(minmax)
+        ax[k].set_ylim(minmax[k])
         
     ax[k].set_xlabel("Number of samples")
         
