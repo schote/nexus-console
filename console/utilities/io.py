979b9159577173629f6d81ec86428fac6771502a
David Schote <david.schote@ptb.de>
2023-08-21 15:14:20 +0200

    Added sequence constructors to yaml loader

--- console/utilities/io.py
+++ console/utilities/io.py
@@ -1,16 +1,22 @@
 import yaml
-
+from pypulseq.opts import Opts
 from console.spcm_control.rx_device import RxCard
 from console.spcm_control.tx_device import TxCard
+from console.pulseq_interpreter.sequence_provider import SequenceProvider
 
-# from pypulseq.opts import Opts
+
+yaml_loader = yaml.SafeLoader
 
 # Add constructors to PyYAML loader
-yaml_loader = yaml.SafeLoader
 yaml_loader.add_constructor(
     "!TxCard", lambda loader, node: TxCard(**loader.construct_mapping(node, deep=True))
 )
 yaml_loader.add_constructor(
     "!RxCard", lambda loader, node: RxCard(**loader.construct_mapping(node, deep=True))
 )
-# yaml_loader.add_constructor("!System", lambda loader, node: Opts(**loader.construct_mapping(node)))
+yaml_loader.add_constructor(
+    "!SequenceProvider", lambda loader, node: SequenceProvider(**loader.construct_mapping(node, deep=True))
+)
+yaml_loader.add_constructor(
+    "!Opts", lambda loader, node: Opts(**loader.construct_mapping(node, deep=True))
+)
