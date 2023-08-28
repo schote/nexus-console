5636abc86944c874e8b7b82e4577ee28c8f77b12
David Schote <david.schote@ptb.de>
2023-08-28 17:57:09 +0200

    Doc path configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../console'))
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
