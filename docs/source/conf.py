9bbbbd6cd06526b7c9d7dabc6171c99d14682a6e
David Schote <david.schote@ptb.de>
2023-08-28 17:51:37 +0200

    Docs path setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__), '..', '..'))))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
