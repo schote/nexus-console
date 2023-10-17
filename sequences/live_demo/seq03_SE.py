db18386f900f8f26041f7b8afae9586fcc37f263
David Schote <david.schote@ptb.de>
2023-10-17 18:16:03 +0200

    Fix live-demo sequences

--- sequences/live_demo/seq03_SE.py
+++ sequences/live_demo/seq03_SE.py
@@ -1,60 +1,68 @@
-import pypulseq as pp
+"""Spin-Echo imaging sequence from ISMRM live-demo."""
+# %%
+# imports
+from pypulseq.opts import Opts
+from pypulseq.Sequence.sequence import Sequence
+from pypulseq import make_block_pulse, make_delay, calc_duration, make_adc
+from math import pi
 
+# %%
 # Create system object with specified parameters
-system = pp.opts(
-    rfRingdownTime=20e-6,
-    rfDeadTime=100e-6,
-    adcDeadTime=20e-6
+system = Opts(
+    rf_ringdown_time=20e-6,
+    rf_dead_time=100e-6,
+    adc_dead_time=20e-6
 )
 
 # Create a new sequence object
-seq = pp.Sequence(system)
+seq = Sequence(system)
 
-Nx = 4096
-Nrep = 1
-adcDur = 51.2e-3
-rfDur = 1000e-6
+n_x = 4096
+n_rep = 10
+adc_duration = 51.2e-3
+rf_duration = 1000e-6
 TR = 250e-3
 TE = 60e-3
 
 # Create non-selective excitation and refocusing pulses
-rf_ex = pp.makeBlockPulse(
-    flipAngle=pi/2,
-    duration=rfDur,
+rf_ex = make_block_pulse(
+    flip_angle=pi/2,
+    duration=rf_duration,
     system=system
 )
 
-rf_ref = pp.makeBlockPulse(
-    flipAngle=pi,
-    duration=rfDur,
+rf_ref = make_block_pulse(
+    flip_angle=pi,
+    duration=rf_duration,
     system=system,
     use='refocusing'
 )
 
 # Define delays and ADC events
-delayTE1 = TE/2 - pp.calcDuration(rf_ex)/2 - pp.calcDuration(rf_ref)/2
-delayTE2 = TE/2 - pp.calcDuration(rf_ref) + rf_ref.delay + pp.calcRfCenter(rf_ref) - adcDur/2
+delay_te_1 = TE/2 - calc_duration(rf_ex)/2 - calc_duration(rf_ref)/2
+delay_te_2 = TE/2 - calc_duration(rf_ref) + rf_ref.delay + calc_duration(rf_ref) - adc_duration/2
 
-adc = pp.makeAdc(Nx, duration=adcDur, system=system, delay=delayTE2)
+adc = make_adc(n_x, duration=adc_duration, system=system, delay=delay_te_2)
 
-delayTR = TR - pp.calcDuration(rf_ex) - delayTE1 - pp.calcDuration(rf_ref)
+delay_tr = TR - calc_duration(rf_ex) - delay_te_1 - calc_duration(rf_ref)
 
-assert delayTE1 >= 0
-assert delayTE2 >= 0
-assert delayTR >= 0
+assert delay_te_1 >= 0
+assert delay_te_2 >= 0
+assert delay_tr >= 0
 
 # Loop over repetitions and define sequence blocks
-for i in range(Nrep):
-    seq.addBlock(rf_ex)
-    seq.addBlock(pp.makeDelay(delayTE1))
-    seq.addBlock(rf_ref)
-    seq.addBlock(adc, pp.makeDelay(delayTR))
+for i in range(n_rep):
+    seq.add_block(rf_ex)
+    seq.add_block(make_delay(delay_te_1))
+    seq.add_block(rf_ref)
+    seq.add_block(adc, make_delay(delay_tr))
 
 # Plot the sequence
 seq.plot()
 
+#%%
 # Check whether the timing of the sequence is compatible with the scanner
-ok, error_report = seq.checkTiming()
+ok, error_report = seq.check_timing()
 
 if ok:
     print('Timing check passed successfully')
@@ -63,11 +71,14 @@ else:
     for err in error_report:
         print(err)
 
+# %%
 # Write the sequence to a pulseq file
-seq.write('se.seq')
+seq.write(f'./export/se_spec_avg-{n_rep}.seq')
 
 # Calculate k-space but only use it to check the TE calculation
-ktraj_adc, t_adc, ktraj, t_ktraj, t_excitation, t_refocusing = seq.calculateKspacePP()
+# ktraj_adc, ktraj, t_excitation, t_refocusing, t_adc = seq.calculate_kspacePP()
 
-assert abs(t_refocusing - t_excitation - TE/2) < 1e-6  # Check that the refocusing happens at 1/2 of TE
-assert abs(t_adc[Nx//2] - t_excitation - TE) < adc.dwell  # Check that the echo happens as close as possible to the middle of the ADC element
+# assert abs(t_refocusing - t_excitation - TE/2) < 1e-6  # Check that the refocusing happens at 1/2 of TE
+# assert abs(t_adc[n_x//2] - t_excitation - TE) < adc.dwell  # Check that the echo happens as close as possible to the middle of the ADC element
+
+# %%
--- sequences/live_demo/seq04_SE_spoiler.py
+++ sequences/live_demo/seq04_SE_spoiler.py
@@ -1,70 +1,78 @@
-import pypulseq as pp
+"""Spin-Echo imaging sequence from ISMRM live-demo."""
+# %%
+# imports
+from pypulseq.opts import Opts
+from pypulseq.Sequence.sequence import Sequence
+from pypulseq import make_block_pulse, make_delay, calc_duration, make_adc, make_trapezoid, calc_rf_center
+from math import pi
 
+# %%
 # Create system object with specified parameters
-system = pp.opts(
-    rfRingdownTime=20e-6,
-    rfDeadTime=100e-6,
-    adcDeadTime=20e-6
+system = Opts(
+    rf_ringdown_time=20e-6,
+    rf_dead_time=100e-6,
+    adc_dead_time=20e-6
 )
 
 # Create a new sequence object
-seq = pp.Sequence(system)
+seq = Sequence(system)
 
-Nx = 4096
-Nrep = 1
-adcDur = 51.2e-3
-rfDur = 1000e-6
-TR = 250e-3
-TE = 60e-3
-spA = 1000  # Spoiler area in 1/m (=Hz/m*s)
+n_x = 4096
+n_rep = 10
+adc_duration = 51.2e-3
+rf_duration = 1000e-6
+tr = 250e-3
+te = 60e-3
+spoiler_area = 1000  # Spoiler area in 1/m (=Hz/m*s)
 
 # Create non-selective excitation and refocusing pulses
-rf_ex = pp.makeBlockPulse(
-    flipAngle=pi/2,
-    duration=rfDur,
+rf_ex = make_block_pulse(
+    flip_angle=pi/2,
+    duration=rf_duration,
     system=system
 )
 
-rf_ref = pp.makeBlockPulse(
-    flipAngle=pi,
-    duration=rfDur,
+rf_ref = make_block_pulse(
+    flip_angle=pi,
+    duration=rf_duration,
     system=system,
     use='refocusing'
 )
-
 # Calculate spoiler gradient, let's put it on X axis for now
-g_sp = pp.makeTrapezoid('x', area=spA, system=system)
-rf_ref.delay = max(pp.calcDuration(g_sp), rf_ref.delay)
+g_sp = make_trapezoid(channel='x', area=spoiler_area, system=system)
+rf_ref.delay = max(calc_duration(g_sp), rf_ref.delay)
 
 # Define delays and ADC events
-delayTE1 = TE/2 - (
-    pp.calcDuration(rf_ex) - pp.calcRfCenter(rf_ex) - rf_ex.delay
-) - rf_ref.delay - pp.calcRfCenter(rf_ref)
+delay_te_1 = te/2 - (
+    calc_duration(rf_ex) - calc_rf_center(rf_ex)[0] - rf_ex.delay
+) - rf_ref.delay - calc_rf_center(rf_ref)[0]
 
-delayTE2 = TE/2 - pp.calcDuration(rf_ref) + rf_ref.delay + pp.calcRfCenter(rf_ref) - adcDur/2
+delay_te_2 = te/2 - calc_duration(rf_ref) + rf_ref.delay + calc_rf_center(rf_ref)[0] - adc_duration/2
 
-assert delayTE2 > pp.calcDuration(g_sp)
+assert delay_te_2 > calc_duration(g_sp)
 
-adc = pp.makeAdc(Nx, duration=adcDur, system=system, delay=delayTE2)
+adc = make_adc(n_x, duration=adc_duration, system=system, delay=delay_te_2)
 
-delayTR = TR - pp.calcDuration(rf_ex) - delayTE1 - pp.calcDuration(rf_ref)
 
-assert delayTE1 >= 0
-assert delayTE2 >= 0
-assert delayTR >= 0
+delay_tr = tr - calc_duration(rf_ex) - delay_te_1 - calc_duration(rf_ref) if n_rep > 1 else 0
+
+assert delay_te_1 >= 0
+assert delay_te_2 >= 0
+assert delay_tr >= 0
 
 # Loop over repetitions and define sequence blocks
-for i in range(Nrep):
-    seq.addBlock(rf_ex)
-    seq.addBlock(pp.makeDelay(delayTE1))
-    seq.addBlock(rf_ref, g_sp)
-    seq.addBlock(adc, g_sp, pp.makeDelay(delayTR))
+for i in range(n_rep):
+    seq.add_block(rf_ex)
+    seq.add_block(make_delay(delay_te_1))
+    seq.add_block(rf_ref, g_sp)
+    seq.add_block(adc, g_sp, make_delay(delay_tr))
 
 # Plot the sequence
 seq.plot()
 
+# %%
 # Check whether the timing of the sequence is compatible with the scanner
-ok, error_report = seq.checkTiming()
+ok, error_report = seq.check_timing()
 
 if ok:
     print('Timing check passed successfully')
@@ -74,10 +82,6 @@ else:
         print(err)
 
 # Write the sequence to a pulseq file
-seq.write('se.seq')
+seq.write(f'./export/se_spec_spoiler_avg-{n_rep}.seq')
 
-# Calculate k-space but only use it to check timing
-ktraj_adc, t_adc, ktraj, t_ktraj, t_excitation, t_refocusing = seq.calculateKspacePP()
-
-assert abs(t_refocusing - t_excitation - TE/2) < 1e-6
-assert abs(t_adc[Nx // 2] - t_excitation - TE) < adc.dwell
+# %%
