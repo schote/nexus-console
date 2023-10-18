2631f23d641d28fb46614bedde74605e69a1efb7
David Schote <david.schote@ptb.de>
2023-10-18 15:03:13 +0200

    Test of post processing chain

--- examples/post_processing.py
+++ examples/post_processing.py
@@ -1,14 +1,15 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
-from console.utilities.processing import apply_ddc
+from console.spcm_control.ddc import apply_ddc
 
 # %%
 # Load acquired SE signal
-filename = "se_signal_05-10-2023"
-filename = ""
+# filename = "se_signal_05-10-2023"
+# raw = np.load(f"/home/schote01/data/spec/{filename}.npy")
 
-raw = np.load(f"data/{filename}.npy")
+filepath = "../experiments/data_1.txt"
+raw = np.loadtxt(filepath)
 
 f_spcm = 20e6   # Spectrum measurement cards sampling frequency; 20 MHz
 f_0 = 2.031e6   # Larmorfrequency; 2.031 MHz
@@ -17,29 +18,29 @@ adc_duration = num_raw_samples / f_spcm
 
 
 # %%
-kernels = list(range(100, 900, 100))
+kernel = 400
 
-fig, ax = plt.subplots(len(kernels), 1, figsize=(10, 15))
+filtered = (apply_ddc(raw, kernel, f_0, f_spcm))
+filtered_fft = np.fft.fftshift(np.fft.fft(filtered))
+fft_freq = np.fft.fftshift(np.fft.fftfreq(filtered_fft.size, kernel/f_spcm))
 
-for i, kernel in enumerate(kernels):
-    filtered = (apply_ddc(raw, kernel, f_0, f_spcm))
-    filtered_fft = np.fft.fftshift(np.fft.fft(filtered))
-    fft_freq = np.fft.fftshift(np.fft.fftfreq(filtered_fft.size, kernel/f_spcm))
-    ax[i].plot(fft_freq, np.abs(filtered_fft), label=f"Kernel size: {kernel}")
-    ax[i].set_xlim([-100e3, 100e3])
-    ax[i].set_ylabel("Abs. FFT [a.u.]")
-    ax[i].legend()
-ax[-1].set_xlabel("Frequency [Hz]")
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+ax.plot(fft_freq, np.abs(filtered_fft), label=f"Kernel size: {kernel}")
+ax.set_xlim([-100e3, 100e3])
+ax.set_ylabel("Abs. FFT [a.u.]")
+ax.legend()
+ax.set_xlabel("Frequency [Hz]")
 
 # %%
-kernel_size = 600
+kernel_size = 200
 
 # Exponential function for demodulation
 demod = np.exp(2j*np.pi*f_0*np.arange(kernel_size)/f_spcm)
 
 # Exponential function for resampling
-x = np.linspace(-1, 1, kernel_size)
-mixer = np.exp(-1 / (1 - x**2)) * np.sinc(x * 2.073 * np.pi)
+# x = np.arange(kernel_size+1) / (kernel_size/2) - 1
+x = np.linspace(-1+1e-16, 1-1e-16, kernel_size)
+mixer = np.exp(-1 / (1 - x**2)) * np.sin(x*2.073*np.pi) / x # np.sinc(x * 2.073 * np.pi)
 
 # Integral for normalization
 norm = np.sum(mixer)
@@ -47,21 +48,33 @@ norm = np.sum(mixer)
 # Kernel function
 kernel = demod * mixer
 
-fig, ax = plt.subplots(1, 3, figsize=(15, 3))
-ax[0].plot(x, kernel)
-ax[0].plot(x, np.abs(kernel), label=f"Kernel size: {kernel_size}")
-ax[0].legend()
+t_kernel = np.arange(kernel_size) / f_spcm
 
-kernel_fft = np.fft.fftshift(np.fft.fft(kernel))
-fft_freq = np.fft.fftshift(np.fft.fftfreq(kernel.size, 1/f_spcm))
+# Plot kernel
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+ax.plot(t_kernel*1e6, kernel)
+ax.plot(t_kernel*1e6, np.abs(kernel), label=f"Kernel size: {kernel_size}")
+ax.legend()
+ax.set_ylabel("Filter amplitude")
+ax.set_xlabel("Time [us]")
 
-ax[1].plot(fft_freq, np.abs(kernel_fft))
-ax[2].plot(fft_freq, np.abs(kernel_fft))
-ax[2].set_xlim([f_0-1e6, f_0+1e6])
+# %%
+# plot raw
+tt = np.arange(raw.size) / f_spcm
 
-ax[1].set_xlabel("Frequency [Hz]")
-ax[2].set_xlabel("Frequency [Hz]")
-ax[0].set_title("Time domain")
-ax[1].set_title("Frequency domain")
-ax[2].set_title("Frequency domain")
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+ax.plot(tt*1e3, raw)
+ax.set_xlabel("Time [ms]")
+ax.set_ylabel("Amplitude [mV]")
+# %%
+# Plot filtered time domain
+kernel_size = 400
+
+filtered = apply_ddc(raw, kernel_size, f_0, f_spcm)
+dwell = filtered.size / f_spcm
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+ax.plot(np.arange(filtered.size)*dwell*1e3, np.abs(filtered), label=f"Kernel size: {kernel_size}")
+ax.set_xlabel("Time [ms]")
+ax.set_ylabel("Amplitude [mV]")
+ax.legend()
 # %%
