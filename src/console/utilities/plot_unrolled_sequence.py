baad8d0c5a021011dbd379910ef03eb6d36b2750
David Schote <david.schote@ptb.de>
2023-10-26 11:08:27 +0200

    Fixed unrolled sequence plot

--- src/console/utilities/plot_unrolled_sequence.py
+++ src/console/utilities/plot_unrolled_sequence.py
@@ -7,7 +7,7 @@ import numpy as np
 from console.pulseq_interpreter.interface_unrolled_sequence import UnrolledSequence
 
 
-def plot_spcm_data(
+def plot_unrolled_sequence(
     sequence: UnrolledSequence, seq_range: tuple[int, int] = (0, -1), use_time: bool = True
 ) -> tuple[matplotlib.figure.Figure, matplotlib.axes.Axes]:
     """Plot replay data for spectrum-instrumentation data in final card format.
@@ -62,21 +62,22 @@ def plot_spcm_data(
     unblanking = gz_signal.astype(np.uint16) >> 15
 
     # Get gradient waveforms
-    gx_signal = 100 * (gx_signal << 1).astype(np.int16) / np.abs(np.iinfo(np.int16).min)
-    gy_signal = 100 * (gy_signal << 1).astype(np.int16) / np.abs(np.iinfo(np.int16).min)
-    gz_signal = 100 * (gz_signal << 1).astype(np.int16) / np.abs(np.iinfo(np.int16).min)
+    rf_signal = rf_signal / np.abs(np.iinfo(np.int16).min)
+    gx_signal = (np.uint16(gx_signal) << 1).astype(np.int16) / np.abs(np.iinfo(np.int16).min)
+    gy_signal = (np.uint16(gy_signal) << 1).astype(np.int16) / np.abs(np.iinfo(np.int16).min)
+    gz_signal = (np.uint16(gz_signal) << 1).astype(np.int16) / np.abs(np.iinfo(np.int16).min)
 
-    axis[0].plot(samples, rf_signal)
-    axis[1].plot(samples, gx_signal)
-    axis[2].plot(samples, gy_signal)
-    axis[3].plot(samples, gz_signal)
+    axis[0].plot(samples, 100 * rf_signal)
+    axis[1].plot(samples, 100 * gx_signal)
+    axis[2].plot(samples, 100 * gy_signal)
+    axis[3].plot(samples, 100 * gz_signal)
     axis[4].plot(samples, adc_gate, label="ADC gate")
     axis[4].plot(samples, unblanking, label="RF unblanking")
 
-    axis[0].set_ylabel("RF (% of max. output)")
-    axis[1].set_ylabel("Gx (% of max. output)")
-    axis[2].set_ylabel("Gy (% of max. output)")
-    axis[3].set_ylabel("Gz (% of max. output)")
+    axis[0].set_ylabel("RF (% of max.)")
+    axis[1].set_ylabel("Gx (% of max.)")
+    axis[2].set_ylabel("Gy (% of max.)")
+    axis[3].set_ylabel("Gz (% of max.)")
     axis[4].set_ylabel("Digital")
     axis[4].legend(loc="upper right")
 
