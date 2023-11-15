e3ef3f599144f31503ac08bc696e33ac802af2e3
David Schote <david.schote@ptb.de>
2023-11-15 17:47:31 +0100

    Move examples

--- test/dev/tse_sorting.py
+++ test/dev/tse_sorting.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # %%
-N = 64
+N = 16
 
-kx = (np.arange(64) - int(N/2))/N
-ky = (np.arange(64) - int(N/2))/N
+kx = (np.arange(N) - int(N/2))/N
+ky = (np.arange(N) - int(N/2))/N
 
 traj_x, traj_y = np.meshgrid(
     kx, ky, indexing="xy"
@@ -20,4 +20,14 @@ ax[0].set_ylabel("k_y")
 ax[0].set_xlabel("k_x")
 ax[1].imshow(distance)
 
+# %%
+
+ordering = np.flip(np.argsort(distance.flatten()))
+
+traj_x_sorted = traj_x.flatten()[ordering]
+traj_y_sorted = traj_y.flatten()[ordering]
+
+max_pts = 10
+
+plt.plot(traj_x_sorted[:max_pts], traj_y_sorted[:max_pts])
 # %%
