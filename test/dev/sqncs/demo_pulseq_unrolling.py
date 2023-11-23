c305c972114b2826c5e914687a4e53b31d408f7f
David Schote <david.schote@ptb.de>
2023-11-23 17:19:55 +0100

    Update sequence unroll demo

--- test/dev/sqncs/demo_pulseq_unrolling.py
+++ test/dev/sqncs/demo_pulseq_unrolling.py
@@ -1,46 +1,29 @@
 """Read and plot unrolled pulseq files with sequence provider class."""
 # %%
 # imports
-from console.utilities.load_config import get_instances
-from console.utilities.spcm_data_plot import plot_spcm_data
-import numpy as np
-import matplotlib.pyplot as plt
-
+from console.pulseq_interpreter.sequence_provider import SequenceProvider
 from console.pulseq_interpreter.interface_unrolled_sequence import UnrolledSequence
+import console.utilities.sequences as sequences
 
 # %%
 # Get sequence provider object and read sequence
-seq, _, _ = get_instances("../device_config.yaml")
+provider = SequenceProvider(rf_to_mvolt=0.005, gpa_gain=[1.0, 1.0, 1.0], gradient_efficiency=[0.4e-3, 0.4e-3, 0.4e-3], output_limits=[200, 6000, 6000, 6000])
 
 # Read sequence file
-# seq.read("../sequences/export/dual-se_spec.seq")
-# seq.read("../sequences/export/fid_proj.seq")
-# seq.read("../sequences/export/gradient_test.seq")
-# seq.read("../sequences/export/tse_low-field.seq")
-# seq.read("../sequences/export/se_proj_400us_sinc_12ms-te.seq")
-seq.read("../sequences/export/se_cartesian_64-pe.seq")
+# provider.read("../sequences/export/dual-se_spec.seq")
+# provider.read("../sequences/export/fid_proj.seq")
+# provider.read("../sequences/export/gradient_test.seq")
+# provider.read("../sequences/export/tse_low-field.seq")
+# provider.read("../sequences/export/se_proj_400us_sinc_12ms-te.seq")
+# provider.read("../sequences/export/se_cartesian_64-pe.seq")
+
+seq = sequences.se_spectrum.constructor(
+    echo_time=20e-3,
+    rf_duration=200e-6,
+    use_sinc=False
+)
+provider.from_pypulseq(seq)
 
 # %%
 f_0 = 2.031e6
-seq.max_amp_per_channel = [200, 6000, 6000, 6000]
-sqnc: UnrolledSequence = seq.unroll_sequence(f_0)
-
-# %%
-
-# fig, _ = plot_spcm_data(sqnc, use_time=False, seq_range=[20e6, 22e6])
-
-# time range in ms
-time_range = (0, 18)
-
-seq_range = [t*1e-3*seq.spcm_freq for t in time_range]
-
-fig, _ = plot_spcm_data(sqnc, use_time=True, seq_range=seq_range)
-fig.show()
-
-# %%
-# Check reference clock
-# seq = np.concatenate(sqnc.seq)
-# reference_clk = -(seq[3::4] >> 15)
-
-# plt.plot(reference_clk[200800:201000])
-# %%
+sqnc: UnrolledSequence = provider.unroll_sequence(f_0)
