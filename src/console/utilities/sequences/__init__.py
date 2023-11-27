fc6fa6148189d5884cb86fd8c20a673d53ea5f44
David Schote <david.schote@ptb.de>
2023-11-27 18:10:09 +0100

    SE sequence with second readout

--- src/console/utilities/sequences/__init__.py
+++ src/console/utilities/sequences/__init__.py
@@ -1,5 +1,5 @@
 """Sequence constructor init file."""
-from console.utilities.sequences import se_projection, se_spectrum, system_settings
+from console.utilities.sequences import se_projection, se_spectrum, se_spectrum_dl, system_settings
 from console.utilities.sequences.calibration import fid_tx_adjust, se_tx_adjust
 from console.utilities.sequences.tse import tse_2d, tse_v1
 from console.utilities.sequences.tse.tse_v1 import Dimensions
@@ -13,5 +13,6 @@ __all__ = [
     "se_spectrum",
     "tse",
     "system_settings",
-    "Dimensions"
+    "se_spectrum_dl",
+    "Dimensions",
 ]
