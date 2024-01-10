1bb46d655acc2824b19c69253d3a34519e546b89
David Schote <david.schote@web.de>
2024-01-10 22:58:02 +0100

    Implementation of 3D tse imaging sequence with test

--- tests/sequence_tests/test_constructors.py
+++ tests/sequence_tests/test_constructors.py
@@ -1,4 +1,5 @@
 """Test sequence constructors contained in the package."""
+import pytest
 from console.utilities import sequences
 
 
@@ -18,6 +19,15 @@ def test_se_projection():
 def test_tse_2d():
     """Test 2D TSE imaging sequence constructor."""
     seq = sequences.tse_2d.constructor(etl=1)[0]
+    assert seq.check_timing()[0] == True
+
+
+@pytest.mark.parametrize("etl", [1, 7])
+@pytest.mark.parametrize("dim", [sequences.Dimensions(64, 32, 1), sequences.Dimensions(64, 32, 32)])
+@pytest.mark.parametrize("te", [40e-3, 60e-3])
+def test_tse_3d(etl, dim, te):
+    """Test 2D TSE imaging sequence constructor."""
+    seq, _ = sequences.tse_3d.constructor(n_enc=dim, etl=etl, echo_time=te)
     assert seq.check_timing()[0] == True
 
 
