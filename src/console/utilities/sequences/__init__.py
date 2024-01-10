f1505d9a90145c1b966763eb2530babfd7777f0f
David Schote <david.schote@web.de>
2024-01-10 23:05:23 +0100

    Fix import

--- src/console/utilities/sequences/__init__.py
+++ src/console/utilities/sequences/__init__.py
@@ -3,7 +3,7 @@ from . import system_settings
 from .calibration import fid_tx_adjust, se_tx_adjust
 from .spectrometry import se_projection, se_spectrum, se_spectrum_dl, t2_relaxation
 from .tse import tse_2d, tse_3d
-from .tse.tse_v1 import Dimensions
+from .tse.tse_3d import Dimensions
 
 __all__ = [
     "fid_tx_adjust",
