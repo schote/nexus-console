4540a88bd62cc3bdbb224aaaec06cc6922872c20
David Schote <david.schote@ptb.de>
2024-02-27 14:54:52 +0100

    Fixed unrolling test

--- tests/sequence_tests/test_unrolling.py
+++ tests/sequence_tests/test_unrolling.py
@@ -3,6 +3,7 @@ import matplotlib
 import numpy as np
 
 from console.pulseq_interpreter.interface_unrolled_sequence import UnrolledSequence
+from console.spcm_control.interface_acquisition_parameter import AcquisitionParameter
 
 
 def test_sequence_provider(seq_provider, test_sequence):
@@ -10,7 +11,8 @@ def test_sequence_provider(seq_provider, test_sequence):
     assert test_sequence.check_timing()[0]
 
     seq_provider.from_pypulseq(test_sequence)
-    unrolled_seq: UnrolledSequence = seq_provider.unroll_sequence(larmor_freq=2e6)
+    params = AcquisitionParameter(larmor_frequency=2e6)
+    unrolled_seq: UnrolledSequence = seq_provider.unroll_sequence(parameter=params)
     fig, ax = seq_provider.plot_unrolled()
 
     assert unrolled_seq.duration == test_sequence.duration()[0]
