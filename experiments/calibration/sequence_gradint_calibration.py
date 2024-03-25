ceeaf57f041f05ed5597718f0076c03742a631b4
Tom <t.o_reilly@lumc.nl>
2024-03-25 17:20:16 +0100

    Acquired gradient monitor

--- experiments/calibration/sequence_gradint_calibration.py
+++ experiments/calibration/sequence_gradint_calibration.py
@@ -4,7 +4,15 @@ import pypulseq as pp
 from console.utilities.sequences.system_settings import raster, system
 
 
-def constructor(fov: float = 0.24, num_samples: int = 120, ramp_duration: float = 200e-6, ro_bandwidth: float = 20e3, delay: float = 0.) -> pp.Sequence:
+def constructor(
+    fov: float = 0.24,
+    num_samples: int = 120,
+    ramp_duration: float = 200e-6,
+    ro_bandwidth: float = 20e3,
+    delay: float = 0.,
+    post_gradient_adc_duration: float = 1e-3
+    ) -> pp.Sequence:
+    
     seq = pp.Sequence(system=system)
     seq.set_definition("Name", "grad-calibration")
 
@@ -19,7 +27,7 @@ def constructor(fov: float = 0.24, num_samples: int = 120, ramp_duration: float 
         fall_time=ramp_duration,
         flat_time=raster(adc_duration, precision=system.grad_raster_time),
     )
-    
+
     grad_y = pp.make_trapezoid(
         channel="y",
         system=system,
@@ -28,7 +36,7 @@ def constructor(fov: float = 0.24, num_samples: int = 120, ramp_duration: float 
         fall_time=ramp_duration,
         flat_time=raster(adc_duration, precision=system.grad_raster_time),
     )
-        
+
     grad_z = pp.make_trapezoid(
         channel="z",
         system=system,
@@ -37,15 +45,15 @@ def constructor(fov: float = 0.24, num_samples: int = 120, ramp_duration: float 
         fall_time=ramp_duration,
         flat_time=raster(adc_duration, precision=system.grad_raster_time),
     )
-    
-    grad_duration = pp.calc_duration(grad_x)
-    
+
+    grad_duration = pp.calc_duration(grad_x) + post_gradient_adc_duration
+
     adc = pp.make_adc(
         system=system,
         num_samples=int(grad_duration/system.adc_raster_time),
         duration=raster(val=grad_duration, precision=system.adc_raster_time),
     )
-    
+
     seq.add_block(pp.make_delay(raster(delay, precision=system.grad_raster_time)))
     seq.add_block(grad_x, grad_y, grad_z, adc)
 
