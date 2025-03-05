a034653af4df13e1e96a4303b8c3b843c401df9a
David Schote <david.schote@ptb.de>
2025-03-05 13:21:02 +0100

    Fixed linter findings with latest version

--- src/console/spcm_control/rx_device.py
+++ src/console/spcm_control/rx_device.py
@@ -420,7 +420,6 @@ class RxCard(SpectrumDevice):
                         gate_data = gate_data[pre_trigger_cut:]
                         self.rx_data.append(gate_data.reshape((self.num_channels.value, gate_sample), order="F"))
 
-
                         # The accumulation of the leftover bytes is positive,
                         # if if the post-trigger event was not fully captured (accumulated sum increases),
                         # or negative if more then the expected data could be read due to lefter bytes
