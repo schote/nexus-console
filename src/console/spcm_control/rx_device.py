2f436dd58c599906e2b7592a5486cc1a57f4e119
David Schote <david.schote@ptb.de>
2025-04-24 14:03:48 +0200

    Removed redundant functions, dataclass

--- src/console/spcm_control/rx_device.py
+++ src/console/spcm_control/rx_device.py
@@ -10,7 +10,7 @@ import numpy as np
 
 import console.spcm_control.spcm.pyspcm as sp
 from console.spcm_control.abstract_device import SpectrumDevice
-from console.spcm_control.spcm.tools import create_dma_buffer, translate_status, type_to_name
+from console.spcm_control.spcm.tools import create_dma_buffer, type_to_name
 
 # Define registers lists
 CH_SELECT = [
@@ -53,18 +53,25 @@ getcontext().prec = 28
 class RxCard(SpectrumDevice):
     """Implementation of RX device."""
 
-    path: str
-    sample_rate: int
-    channel_enable: list[int]
-    max_amplitude: list[int]
-    impedance_50_ohms: list[int]
-
     __name__: str = "RxCard"
 
-    def __post_init__(self):
+    def __init__(
+        self,
+        path: str,
+        sample_rate: int,
+        channel_enable: list[int],
+        max_amplitude: list[int],
+        impedance_50_ohms: list[int],
+    ) -> None:
         """Execute after init function to do further class setup."""
         self.log = logging.getLogger(self.__name__)
-        super().__init__(self.path, log=self.log)
+        super().__init__(path, log=self.log)
+
+        self.sample_rate = sample_rate
+        self.channel_enable = channel_enable
+        self.max_amplitude = max_amplitude
+        self.impedance_50_ohms = impedance_50_ohms
+
         self.num_channels = sp.int32(0)
         self.card_type = sp.int32(0)
 
@@ -75,17 +82,8 @@ class RxCard(SpectrumDevice):
         self.pre_trigger = 8
         self.post_trigger = 4096
 
-        self.rx_data = []
+        self.rx_data: list = []
         self.rx_scaling = [amp / (2**15) for amp in self.max_amplitude]
-
-    def dict(self) -> dict:
-        """Returnt class variables which are json serializable as dictionary.
-
-        Returns
-        -------
-            Dictionary containing class variables.
-        """
-        return super().dict()
 
     def setup_card(self):
         """Set up spectrum card in transmit (TX) mode.
@@ -212,7 +210,7 @@ class RxCard(SpectrumDevice):
         sp.spcm_dwSetParam_i32(self.card, sp.SPC_TIMEOUT, 10)
 
         self.log.debug("Device setup completed")
-        self.log_card_status()
+        _ = self.get_status()
 
     def start_operation(self):
         """Start card operation."""
@@ -438,36 +436,3 @@ class RxCard(SpectrumDevice):
                         break
 
         self.log.debug("Card operation stopped")
-
-    def get_status(self) -> int:
-        """Get the current card status.
-
-        Returns
-        -------
-            String with status description.
-        """
-        try:
-            if not self.card:
-                raise ConnectionError("No device found")
-        except ConnectionError as err:
-            self.log.exception(err, exc_info=True)
-            raise err
-        status = sp.int32(0)
-        sp.spcm_dwGetParam_i32(self.card, sp.SPC_M2STATUS, byref(status))
-        return status.value
-
-    def log_card_status(self, include_desc: bool = False) -> None:
-        """Log current card status.
-
-        The status is represented by a list. Each entry represents a possible card status in form
-        of a (sub-)list. It contains the status code, name and (optional) description of the spectrum
-        instrumentation manual.
-
-        Parameters
-        ----------
-        include_desc, optional
-            Flag which indicates if description string should be contained in status entry, by default False
-        """
-        msg, _ = translate_status(self.get_status(), include_desc=include_desc)
-        status = {key: val for val, key in msg.values()}
-        self.log.debug("Card status:\n%s", status)
