Fixed moving average and cic tests

tests/acquisition/test_decimation.py
@@ -1,9 +1,9 @@
 """Test digital down converter (DDC) functions."""
 import numpy as np
 import pytest
+from scipy import signal
 
 from console.utilities.ddc import filter_cic_fir_comp, filter_moving_average
-from scipy import signal
 
 
 @pytest.mark.parametrize("coils", [1, 2, 4])
@@ -12,17 +12,14 @@ from scipy import signal
 @pytest.mark.parametrize("decimation", [100, 200, 400])
 @pytest.mark.parametrize("overlap", [2, 4])
 def test_moving_average_filter(coils, phase_encoding, num_samples, decimation, overlap, random_acquisition_data):
-    """Test moving average filter for various parameter configurations."""
+    """Test moving average filter for various parameter configurations with FIR as reference."""
     input_data = random_acquisition_data(1, coils, phase_encoding, num_samples)
-    processed = filter_moving_average(input_data, decimation=decimation, overlap=overlap)
+    processed_avg = filter_moving_average(input_data, decimation=decimation, overlap=overlap)
+    processed_fir = signal.decimate(input_data, q=decimation, ftype="fir")
 
-    proc_avg, proc_coils, proc_pe, proc_samples = processed.shape
-
-    assert proc_avg == 1
-    assert proc_coils == coils
-    assert proc_pe == phase_encoding
-    assert proc_samples == round(num_samples / decimation)
-    assert np.iscomplex(processed).all()
+    assert processed_avg.shape == processed_fir.shape
+    assert np.iscomplex(processed_avg).all()
+    assert np.iscomplex(processed_fir).all()
 
 
 @pytest.mark.parametrize("coils", [1, 2, 4])
@@ -31,17 +28,14 @@ def test_moving_average_filter(coils, phase_encoding, num_samples, decimation, o
 @pytest.mark.parametrize("decimation", [100, 200, 400])
 @pytest.mark.parametrize("filter_stages", [2, 3, 5])
 def test_cic_fir_comp(coils, phase_encoding, num_samples, decimation, filter_stages, random_acquisition_data):
-    """Test CIC FIR filter composition."""
+    """Test CIC FIR filter composition with FIR as reference."""
     input_data = random_acquisition_data(1, coils, phase_encoding, num_samples)
-    processed = filter_cic_fir_comp(input_data, decimation=decimation, number_of_stages=filter_stages)
+    processed_cic = filter_cic_fir_comp(input_data, decimation=decimation, number_of_stages=filter_stages)
+    processed_fir = signal.decimate(input_data, q=decimation, ftype="fir")
 
-    proc_avg, proc_coils, proc_pe, proc_samples = processed.shape
+    assert processed_cic.shape == processed_fir.shape
+    assert np.iscomplex(processed_cic).all()
 
-    assert proc_avg == 1
-    assert proc_coils == coils
-    assert proc_pe == phase_encoding
-    assert proc_samples == num_samples // decimation
-    assert np.iscomplex(processed).all()
 
 @pytest.mark.parametrize("num_raw_samples", [80640, 128000])
 def test_decimated_shapes(num_raw_samples, random_acquisition_data):