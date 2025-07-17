6d10def75a02cd66bd3edb2a3f97a0ba5f4cfbb1
Toreil <toreil@me.com>
2025-07-17 13:16:54 +0200

    Linting fix

--- examples/acquisition_manager.py
+++ examples/acquisition_manager.py
@@ -38,7 +38,7 @@ with AcquisitionControlManager() as mngr:
 scan_data = np.array([rx_data.processed_data for rx_data in data.receive_data])
 num_coils = np.size(scan_data, 1)
 
-fig, ax = plt.subplots(1, num_coils, figsize=(5*num_coils, 5))
+fig, ax = plt.subplots(1, num_coils, figsize=(5 * num_coils, 5))
 for coil in range(num_coils):
     ax[coil].plot(np.abs(scan_data[:, coil, :]).T)
     ax[coil].set_xlabel("Sample")
