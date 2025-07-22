3d52eb6ccc51bcb23b1277ff58889ab3f859b606
David Schote <david.schote@ptb.de>
2025-07-22 15:37:52 +0200

    Prevent warning in tse3d sequence

--- src/console/utilities/sequences/tse/tse_3d.py
+++ src/console/utilities/sequences/tse/tse_3d.py
@@ -238,17 +238,19 @@ def constructor(
     # Definition of RF pulses
     rf_90 = pp.make_block_pulse(
         system=system,
+        delay=max(0, system.rf_dead_time),
         flip_angle=excitation_angle,
         phase_offset=excitation_phase,
         duration=rf_duration,
-        use="excitation"
+        use="excitation",
     )
     rf_180 = pp.make_block_pulse(
         system=system,
+        delay=max(0, system.rf_dead_time),
         flip_angle=refocussing_angle,
         phase_offset=refocussing_phase,
         duration=rf_duration,
-        use="refocusing"
+        use="refocusing",
     )
     if inversion_pulse:
         rf_inversion = pp.make_block_pulse(
