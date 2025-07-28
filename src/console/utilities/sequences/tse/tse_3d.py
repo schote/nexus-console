785ffecb1473de30af638900a1dc99581b27c7e7
David Schote <david.schote@ptb.de>
2025-07-28 18:47:51 +0200

    Use "SET" type label for IMA

--- src/console/utilities/sequences/tse/tse_3d.py
+++ src/console/utilities/sequences/tse/tse_3d.py
@@ -378,9 +378,9 @@ def constructor(
             # Cast index values from int32 to int, otherwise make_label function complains
             label_pe1 = pp.make_label(type="SET", label="LIN", value=int(pe_indices[0]))
             label_pe2 = pp.make_label(type="SET", label="PAR", value=int(pe_indices[1]))
-            label_echo = pp.make_label(type='SET', label="ECO", value=int(echo_num + 1))
-            label_tr = pp.make_label(type='SET', label="REP", value=int(train_num + 1))
-            label_img = pp.make_label(type="INC", label="IMA", value=True)
+            label_echo = pp.make_label(type="SET", label="ECO", value=int(echo_num + 1))
+            label_tr = pp.make_label(type="SET", label="REP", value=int(train_num + 1))
+            label_img = pp.make_label(type="SET", label="IMA", value=True)
             seq.add_block(grad_ro, adc, label_pe1, label_pe2, label_tr, label_echo, label_img)
 
             seq.add_block(
