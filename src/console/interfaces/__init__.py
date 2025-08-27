13c71432c4bfb98be5f4c6bc1de5319a5f83bb28
David Schote <david.schote@ptb.de>
2025-08-27 15:13:55 +0200

    Import interfaces in init

--- src/console/interfaces/__init__.py
+++ src/console/interfaces/__init__.py
@@ -1,1 +1,16 @@
 """Interfaces init file."""
+from .dimensions import Dimensions
+from .acquisition_data import AcquisitionData
+from .rx_data import RxData
+from .unrolled_sequence import UnrolledSequence
+from .acquisition_parameter import AcquisitionParameter
+from .enums import DDCMethod
+
+__all__ = [
+    "Dimensions",
+    "AcquisitionData",
+    "RxData",
+    "UnrolledSequence",
+    "AcquisitionParameter",
+    "DDCMethod"
+]
