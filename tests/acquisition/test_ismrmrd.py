1c05d425dcadb101dfc16af731304f6d02e7ec79
David Schote <david.schote@ptb.de>
2025-08-27 15:16:32 +0200

    Added test for 1D ismrmrd export

--- tests/acquisition/test_ismrmrd.py
+++ tests/acquisition/test_ismrmrd.py
@@ -2,10 +2,12 @@
 import tempfile
 from pathlib import Path
 
+import ismrmrd
 import pytest
 
 from console.interfaces.acquisition_data import AcquisitionData
 from console.interfaces.dimensions import Dimensions
+from console.utilities.data import write_1d_mrd
 from console.utilities.sequences import tse_3d
 
 
@@ -43,3 +45,32 @@ def test_tse_3d(trajectory_type, dim, random_complex_data, acquisition_parameter
     acq_folder = Path(tmp_dir) / acq_data.meta["folder_name"]
     acq_data_files = [f.name for f in acq_folder.iterdir() if f.is_file()]
     assert "data.mrd" in acq_data_files
+
+
+@pytest.mark.parametrize("num_coils", [1, 2, 4])
+@pytest.mark.parametrize("num_averages", [1, 5])
+def test_write_1d_ismrmrd(random_acquisition_data, num_coils: int, num_averages: int) -> None:
+    """Test ismrmrd export for 1D data."""
+    receive_data = random_acquisition_data(
+        num_coils=num_coils,
+        num_samples=1000,
+        num_acquisitions=1,
+        num_averages=num_averages,
+    )
+
+    header = ismrmrd.xsd.ismrmrdHeader()
+    header.experimentalConditions = ismrmrd.xsd.experimentalConditionsType(receive_data[0].larmor_frequency)
+
+    tmp_dir = tempfile.mkdtemp()
+    mrd_path = write_1d_mrd(
+        data=receive_data,
+        header=header,
+        dataset_path=Path(tmp_dir) / "raw_data.mrd",
+    )
+
+    with ismrmrd.File(mrd_path, 'r') as fh:
+        dataset = fh['dataset']
+        acquisitions = dataset.acquisitions[:]
+
+    assert len(acquisitions) == len(receive_data)
+
