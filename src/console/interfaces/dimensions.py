009061927c631fba2ca5313862e9c7832d996c50
David Schote <david.schote@ptb.de>
2025-11-14 12:19:01 +0100

    Make dimensions a normal dataclass

--- src/console/interfaces/dimensions.py
+++ src/console/interfaces/dimensions.py
@@ -1,66 +1,93 @@
 """Interface class for dimensions."""
+from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
 
 
-@dataclass(frozen=True)
+@dataclass
 class Dimensions:
     """Dataclass for definition of dimensional parameters."""
 
-    x: float | int  # pylint: disable=invalid-name
+    x: int | float  # pylint: disable=invalid-name
     """X dimension."""
 
-    y: float | int  # pylint: disable=invalid-name
+    y: int | float  # pylint: disable=invalid-name
     """Y dimension."""
 
-    z: float | int  # pylint: disable=invalid-name
+    z: int | float  # pylint: disable=invalid-name
     """Z dimension."""
 
     @classmethod
-    def from_dict(cls, dim: dict[str, float | int]) -> "Dimensions":
+    def from_dict(cls, dim: dict[str, int | float]) -> Dimensions:
         """Create a dimensions instance from dictionary."""
-        return cls(x=dim["x"], y=dim["y"], z=dim["z"])
+        return cls(**dim)
 
-    def dict(self) -> dict[str, float | int]:
-        """Create dictionary of dimensions object."""
-        return {"x": self.x, "y": self.y, "z": self.z}
+    def to_dict(self) -> dict[str, int | float]:
+        """Convert to a nested dictionary."""
+        return asdict(self)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return custom representation string."""
         return f"x={self.x}, y={self.y}, z={self.z}"
 
-    def __mul__(self, other) -> "Dimensions":
+    # ---------------- Arithmetic ---------------- #
+    # Note: The arguments do not have an int typ annotation, since float also allows ints.
+    # See https://docs.astral.sh/ruff/rules/redundant-numeric-union/
+
+    def __mul__(self, other: float | Dimensions) -> Dimensions:
         """Multiply dimension."""
         if isinstance(other, Dimensions):
             return Dimensions(x=self.x * other.x, y=self.y * other.y, z=self.z * other.z)
-        else:
-            return Dimensions(x=self.x * other, y=self.y * other, z=self.z * other)
+        return Dimensions(x=self.x * other, y=self.y * other, z=self.z * other)
 
-    def __add__(self, other) -> "Dimensions":
-        """Multiply dimension."""
+    def __add__(self, other: float | Dimensions) -> Dimensions:
+        """Add dimension."""
         if isinstance(other, Dimensions):
             return Dimensions(x=self.x + other.x, y=self.y + other.y, z=self.z + other.z)
-        else:
-            return Dimensions(x=self.x + other, y=self.y + other, z=self.z + other)
+        return Dimensions(x=self.x + other, y=self.y + other, z=self.z + other)
 
-    def __sub__(self, other) -> "Dimensions":
-        """Multiply dimension."""
+    def __sub__(self, other: float | Dimensions) -> Dimensions:
+        """Subtract dimension."""
         if isinstance(other, Dimensions):
             return Dimensions(x=self.x - other.x, y=self.y - other.y, z=self.z - other.z)
-        else:
-            return Dimensions(x=self.x - other, y=self.y - other, z=self.z - other)
+        return Dimensions(x=self.x - other, y=self.y - other, z=self.z - other)
 
-    def __imul__(self, _):
+    def __imul__(self, other: float | Dimensions) -> Self:
         """In-place multiplication."""
-        raise TypeError("In-place operations are not supported.\
-            Use 'dimensions = dimensions * value' instead.")
+        if isinstance(other, Dimensions):
+            self.x *= other.x
+            self.y *= other.y
+            self.z *= other.z
+        else:
+            self.x *= other
+            self.y *= other
+            self.z *= other
+        return self
 
-    def __iadd__(self, _):
+    def __iadd__(self, other: float | Dimensions) -> Self:
         """In-place addition."""
-        raise TypeError("In-place operations are not supported.\
-            Use 'dimensions = dimensions + value' instead.")
+        if isinstance(other, Dimensions):
+            self.x += other.x
+            self.y += other.y
+            self.z += other.z
+        else:
+            self.x += other
+            self.y += other
+            self.z += other
+        return self
 
-    def __isub__(self, _):
-        """In-place addition."""
-        raise TypeError("In-place operations are not supported.\
-            Use 'dimensions = dimensions - value' instead.")
+    def __isub__(self, other: float | Dimensions) -> Self:
+        """In-place subtraction."""
+        if isinstance(other, Dimensions):
+            self.x -= other.x
+            self.y -= other.y
+            self.z -= other.z
+        else:
+            self.x -= other
+            self.y -= other
+            self.z -= other
+        return self
