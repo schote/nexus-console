c614ae2da3e7689c434f31e85540dbb2c33e2e03
David Schote <david.schote@ptb.de>
2025-11-26 15:08:13 +0100

    Fixed write MRD method to passcurrent tests

--- src/console/utilities/data/write_acquisition_to_mrd.py
+++ src/console/utilities/data/write_acquisition_to_mrd.py
@@ -30,25 +30,24 @@ def write_acquisition_to_mrd(
         acq.slice_dir[2] = 1.0
 
         trajectory_position = 0
-        count_unsaved = 0
+        none_counter = 0
 
         # Calculate k-space trajectory from sequence
         trajectory = sequence.calculate_kspace()[0]
-        # Trajectory always has shape (3, num_ro), thus accounting the number of non-zero rows (axis=0),
-        # yields the number of active gradients.
-        # No gradient active -> n_dims = 0, RO + PE -> n_dims = 2, ...
-        n_dims = sum(np.all(trajectory != 0, axis=1))
+        # Trajectory always has shape (3, num_ro), the following line gets all axes which contain values unequal 0
+        # The sum of traj_dims, yields the trajectory dimension, while traj_dims is used to mask the trajectory.
+        traj_dims = np.all(trajectory != 0, axis=1)
 
         for k, rx_data in enumerate(data):
-            if rx_data.labels is None or rx_data.processed_data is None:
-                count_unsaved += 1
+            if rx_data.processed_data is None:
+                none_counter += 1
                 continue
 
             acq.clear_all_flags()
             acq.scan_counter = k
             # Resize each acquisition to the individual number of sample points and active channels
             num_coils = rx_data.processed_data.shape[0]
-            acq.resize(number_of_samples=rx_data.num_samples, active_channels=num_coils, trajectory_dimensions=n_dims)
+            acq.resize(number_of_samples=rx_data.num_samples, active_channels=num_coils, trajectory_dimensions=traj_dims.sum())
             # Assume the center sample is the middle of the data
             acq.center_sample = rx_data.num_samples // 2
             # Readout bandwidth, as time between samples in microseconds
@@ -64,18 +63,19 @@ def write_acquisition_to_mrd(
             mrd_helper.set_mrd_flags(acq, rx_data)
 
             # If trajectory is available
-            traj = trajectory[:, trajectory_position:trajectory_position + rx_data.num_samples].T
-            acq.traj[:] = traj
-            trajectory_position += rx_data.num_samples
+            if traj_dims.sum() > 0:
+                traj = trajectory[traj_dims, trajectory_position:trajectory_position + rx_data.num_samples].T
+                acq.traj[:] = traj
+                trajectory_position += rx_data.num_samples
 
             # Set the data and append
             acq.data[:] = rx_data.processed_data
             dataset.append_acquisition(acq)
 
     # Log warning if unlabeled acquisitions were found
-    if count_unsaved > 0:
+    if none_counter > 0:
         log.warning(
-            "%i/%i acquisitions are unlabeled/none and could not be exported.", count_unsaved, len(data),
+            "%i/%i acquisitions are unlabeled/none and could not be exported.", none_counter, len(data),
         )
 
     if not dataset_path.is_file():
