e298293fc52270064f790755703a13addfddd77f
David Schote <42237900+schote@users.noreply.github.com>
2025-11-27 15:00:22 +0100

    Update src/console/utilities/data/__init__.py
    Co-authored-by: Christoph Kolbitsch <christoph.kolbitsch@ptb.de>

--- src/console/utilities/data/__init__.py
+++ src/console/utilities/data/__init__.py
@@ -8,7 +8,6 @@ from .write_acquisition_to_mrd import write_acquisition_to_mrd
 from .mrd_helper import get_nexus_acquisition_system, set_mrd_counters
 
 __all__ = [
-    # "write_1d_mrd",
     "write_acquisition_to_mrd",
     "get_nexus_acquisition_system",
     "set_mrd_counters"
